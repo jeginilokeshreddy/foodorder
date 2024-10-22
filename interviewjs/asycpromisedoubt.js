982b0b77df904aca5d2f0d30a3d102b2022ba715
jeginilokeshreddy <jeginilokesh@gmail.com>
2024-10-23 00:21:33 +0530

    what is the use of the async promise object this is my doubt

---
+++ interviewjs/asycpromisedoubt.js
@@ -0,0 +1,14 @@
+async function sum(){
+      let details=await fetch() 
+             
+           
+}
+console.log(sum())
+
+ function retunpromiseobject() {
+    let data = new Promise((resolve, reject) => {})
+    return data
+ }
+console.log(retunpromiseobject())
+retunpromiseobject().then(()=>{}).catch(()=>{}) 
+// what is the use of the async promise object this is my doubt
