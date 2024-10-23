408d3aaaec579bf066e3caf915f565f926794a51
jeginilokeshreddy <jeginilokesh@gmail.com>
2024-10-24 01:53:14 +0530

    with the help of api end point how we will connect frontend and backend

--- csword-reactjs/apiendpoint.js
+++ csword-reactjs/apiendpoint.js
@@ -1,4 +1,4 @@
-// https://chatgpt.com/share/67195aa7-e4fc-8001-b3a5-5b1642ef813c whith the help of api end point how we will connect frontend and backend 
+// https://chatgpt.com/share/67195aa7-e4fc-8001-b3a5-5b1642ef813c with the help of api end point how we will connect frontend and backend 
 
 
 
