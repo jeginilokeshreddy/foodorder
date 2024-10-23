32f8975bcd9b2dbe84aa110349dab100f930b64b
jeginilokeshreddy <jeginilokesh@gmail.com>
2024-10-24 01:52:28 +0530

    with the help of api end point how we will connect frontend and backend

---
+++ csword-reactjs/apiendpoint.js
@@ -0,0 +1,15 @@
+// https://chatgpt.com/share/67195aa7-e4fc-8001-b3a5-5b1642ef813c whith the help of api end point how we will connect frontend and backend 
+
+
+
+
+
+// api end point 
+
+// https://h:p/endpoint
+
+// https://host:port/endpoint
+// https://localhost:port/endpoint
+
+// localhost means ip address
+
