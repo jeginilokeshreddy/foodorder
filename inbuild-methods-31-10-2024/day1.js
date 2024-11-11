5e321e48d53535d1078ef227a04acb61d69727c0
jeginilokeshreddy <jeginilokesh@gmail.com>
2024-11-11 08:35:25 +0530

    how to learn inbuild methods

---
+++ inbuild-methods-31-10-2024/day1.js
@@ -0,0 +1,63 @@
+// ---------------------------Script-refference---------------------------
+        // script is my reffernce to control my thoughts 
+        //what data i nedd to record
+        //what work i nedd to do in the give time
+        //what flow or process i nedd to follow to complete the word
+        // once scriprt complred according to the script recode and edit ehte video
+// procedure
+// --------------
+// step-1 
+// step-2 -
+
+// step-work complted
+// Category
+// ------
+// street hotel 
+
+
+// reffernce always deals our workout (endpoint)
+
+// reffernce --indicate work 
+// work is our output 
+// output is our endpoint
+
+// script is how many steps we nedd to follow to complted work
+
+// ---------------------------------------------------------------
+
+
+// In javascript we have array data type.
+// This array data type consisting in build methods in javascript.
+// Today,we are going to learn map method filtre method
+// both map and filter method are are used for itterating of the array elemnets
+// 1)map methods is used for ittterate and manipulated array elments from the array ,here length of arraay is constan
+// 2)filter methods is used for ittterate  array elments from the array ,here length of arraay is will vary because fillter will return only true elements fromthe array 
+// 3)muitable and im muitable 
+// 4)the method which will over right the original array then it become muitable
+// -------------------------
+
+// -----------------
+// 1)syntax of map method -->array.map(()=>{})
+// 2)map will take call back as arufument
+// 3)call back will take three arugruments
+// 4)syntax :array.map((item,index,elemnets)=>{
+//     business logic
+// })
+// 5)call will tak 3 arugument (one is required ,2 )
+
+
+
+
+// -----------------------how to learn any method--------------------
+
+
+// 
+// 1))what is the use of this method
+//2)method belong to which data type
+//3)syntax of any method
+// 4)how many aruguments it will take
+// 5)how many are optional how many are required  arugruments
+// 6)what data type of aruguments
+// 7)from where we will receive data to  this arugumnet
+//8)what is the use of each arugument
+// 9)what output method will return
